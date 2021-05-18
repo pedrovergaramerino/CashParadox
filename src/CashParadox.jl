6378778f896bd66b4ab115b26d26b2f4fab2d77e
Pedro Vergara Merino <pedro.vergaramerino@sciencespo.fr>
2021-05-18 19:22:11 +0200

    fix data in module

--- src/CashParadox.jl
+++ src/CashParadox.jl
@@ -20,144 +20,146 @@ mutable struct load_data
     ρ::Vector{Float64}
     ddata::Vector{Float64}
 
-function load_data(irflag::Int64,flag::Int64)
-    dict=matread(joinpath("data.mat"))
-    dictir=matread(joinpath("ir.mat"))
-    can=dict["can"]
-    uk=dict["uk"]
-    usa=dict["usa"]
-    aus=dict["aus"]
-    ir_can=dictir["ir_can"]
-    ir_uk=dictir["ir_uk"]
-    ir_usa=dictir["ir_usa"]
-    ir_aus=dictir["ir_aus"]
-    if irflag==1
-        if flag==0 #CANADA
-            year=can[:,1]
-            ir=can[:,2]
-            ddata=[0 0.49 0.782 0.822 0.856]' #Data on access to credit
-            yt=[1960 1980 1999 2005 2012]'
-            ρ=can[:,3]
-        elseif flag==1 #USA
-            year=usa[:,1]
-            ir=usa[:,2]
-            ddata=[0 16.3 38.3 43.0 55.8 62.2 66.5 68 72.6 71.5]'/100
-            yt=[1960 1970 1977 1983 1989 1992 1995 1998 2001 2004]'
-            ρ=usa[:,3]
-        elseif flag==2 #AUSTRALIA
-            year=aus[:,1]
-            ir=aus[:,2]
-            ddata=[0 0.520354595 0.537666422 0.57032816 0.594028535 0.622316221 0.65072652 0.687205136 0.694317256 0.73605656 0.760156768 0.792611398 0.841961623 0.878037024 0.905946326 0.90592637 0.897747613 0.904361411 0.90064194]'
-            yt= vcat([1965], collect(1994:2011))
-            ρ=aus[:,3]
-        else #UK 
-            uk=uk[5:end,:]
-            year=uk[:,1]
-            ir=uk[:,2]
-            ddata=[0 0.5 0.53 0.65 0.62 0.64 0.62 0.61  0.6]'
-            yt=[1965 1999  2000  2005  2009  2010  2011  2012  2013]'
-            ρ=uk[:,3]
+    function load_data(irflag::Int64,flag::Int64)
+        datamat= joinpath(@__DIR__,"data","data.mat")
+        datair= joinpath(@__DIR__,"data","ir.mat")
+        dict=matread(datamat)
+        dictir=matread(datair)
+        can=dict["can"]
+        uk=dict["uk"]
+        usa=dict["usa"]
+        aus=dict["aus"]
+        ir_can=dictir["ir_can"]
+        ir_uk=dictir["ir_uk"]
+        ir_usa=dictir["ir_usa"]
+        ir_aus=dictir["ir_aus"]
+        if irflag==1
+            if flag==0 #CANADA
+                year=can[:,1]
+                ir=can[:,2]
+                ddata=[0 0.49 0.782 0.822 0.856]' #Data on access to credit
+                yt=[1960 1980 1999 2005 2012]'
+                ρ=can[:,3]
+            elseif flag==1 #USA
+                year=usa[:,1]
+                ir=usa[:,2]
+                ddata=[0 16.3 38.3 43.0 55.8 62.2 66.5 68 72.6 71.5]'/100
+                yt=[1960 1970 1977 1983 1989 1992 1995 1998 2001 2004]'
+                ρ=usa[:,3]
+            elseif flag==2 #AUSTRALIA
+                year=aus[:,1]
+                ir=aus[:,2]
+                ddata=[0 0.520354595 0.537666422 0.57032816 0.594028535 0.622316221 0.65072652 0.687205136 0.694317256 0.73605656 0.760156768 0.792611398 0.841961623 0.878037024 0.905946326 0.90592637 0.897747613 0.904361411 0.90064194]'
+                yt= vcat([1965], collect(1994:2011))
+                ρ=aus[:,3]
+            else #UK 
+                uk=uk[5:end,:]
+                year=uk[:,1]
+                ir=uk[:,2]
+                ddata=[0 0.5 0.53 0.65 0.62 0.64 0.62 0.61  0.6]'
+                yt=[1965 1999  2000  2005  2009  2010  2011  2012  2013]'
+                ρ=uk[:,3]
+            end
+        else
+            if flag==0 #CANADA
+                yr0=ir_can[:,1]
+                if irflag==2
+                    ir0=ir_can[:,3] #DLM of actual inflation
+                else
+                    ir0=ir_can[:,4] #Term structure based
+                end
+                ρ0=ir_can[:,6]
+                idx=Vector{Bool}(undef,length(ir0))
+                for i in 1:length(ir0)
+                    if (isnan(ρ0[i])|isnan(ir0[i]))
+                        idx[i]=0
+                    else
+                        idx[i]=1
+                    end
+                end
+                year=yr0[idx]
+                ir=ir0[idx]*100
+                ρ=ρ0[idx]
+                ddata=ddata=[0,0.49,0.782,0.822,0.856]' #Data on access to credit
+                yt=[1960,1980,1999,2005,2012]'
+            elseif flag==1 #USA
+                yr0=ir_usa[:,1]
+                if irflag==2
+                    ir0=ir_usa[:,3] #DLM of actual inflation
+                else
+                    ir0=ir_usa[:,4] #Term structure based
+                end
+                ρ0=ir_usa[:,7]
+                idx=Vector{Bool}(undef,length(ir0))
+                for i in 1:length(ir0)
+                    if (isnan(ρ0[i])|isnan(ir0[i]))
+                        idx[i]=0
+                    else
+                        idx[i]=1
+                    end
+                end
+                year=yr0[idx]
+                ir=ir0[idx]*100
+                ρ=ρ0[idx]
+                ddata=[0.0, 16.3, 38.3, 43.0, 55.8, 62.2, 66.5, 68.0, 72.6, 71.5]'/100
+                yt=[1960, 1970, 1977, 1983, 1989, 1992, 1995, 1998 ,2001 ,2004]'
+            elseif flag==2 #AUSTRALIA
+                yr0=ir_aus[:,1]
+                if irflag==2
+                    ir0=ir_aus[:,3] #DLM of actual inflation
+                else
+                    ir0=ir_aus[:,4] #Term structure based
+                end
+                ρ0=ir_aus[:,7]
+                idx=Vector{Bool}(undef,length(ir0))
+                for i in 1:length(ir0)
+                    if (isnan(ρ0[i])|isnan(ir0[i]))
+                        idx[i]=0
+                    else
+                        idx[i]=1
+                    end
+                end
+                year=yr0[idx]
+                ir=ir0[idx]*100
+                ρ=ρ0[idx] 
+                ddata=[0 0.520354595 0.537666422 0.57032816 0.594028535 0.622316221 0.65072652 0.687205136 0.694317256 0.73605656 0.760156768 0.792611398 0.841961623 0.878037024 0.905946326 0.90592637 0.897747613 0.904361411 0.90064194]'
+                yt=vcat([1965], collect(1994:2011))  
+            else #UK
+                yr0=ir_uk[:,1]
+                if irflag==2
+                    ir0=ir_uk[:,3]
+                else
+                    ir0=ir_uk[:,4]
+                end
+                ρ0=ir_uk[:,6]
+                idx=Vector{Bool}(undef,length(ir0))
+                for i in 1:length(ir0)
+                    if (isnan(ρ0[i])|isnan(ir0[i]))
+                        idx[i]=0
+                    else
+                        idx[i]=1
+                    end
+                end
+                year=yr0[idx]
+                ir=ir0[idx]*100
+                ρ=ρ0[idx]
+                year=year[5:end]
+                ir=ir[5:end]
+                ρ=ρ[5:end]
+                ddata=[0 0.5 0.53 0.65 0.62 0.64 0.62 0.61  0.6]'
+                yt=[1965 1999  2000  2005  2009  2010  2011  2012  2013]'
+            end
         end
-    else
-        if flag==0 #CANADA
-            yr0=ir_can[:,1]
-            if irflag==2
-                ir0=ir_can[:,3] #DLM of actual inflation
-            else
-                ir0=ir_can[:,4] #Term structure based
-            end
-            ρ0=ir_can[:,6]
-            idx=Vector{Bool}(undef,length(ir0))
-            for i in 1:length(ir0)
-                if (isnan(ρ0[i])|isnan(ir0[i]))
-                    idx[i]=0
-                else
-                    idx[i]=1
-                end
-            end
-            year=yr0[idx]
-            ir=ir0[idx]*100
-            ρ=ρ0[idx]
-            ddata=ddata=[0,0.49,0.782,0.822,0.856]' #Data on access to credit
-            yt=[1960,1980,1999,2005,2012]'
-        elseif flag==1 #USA
-            yr0=ir_usa[:,1]
-            if irflag==2
-                ir0=ir_usa[:,3] #DLM of actual inflation
-            else
-                ir0=ir_usa[:,4] #Term structure based
-            end
-            ρ0=ir_usa[:,7]
-            idx=Vector{Bool}(undef,length(ir0))
-            for i in 1:length(ir0)
-                if (isnan(ρ0[i])|isnan(ir0[i]))
-                    idx[i]=0
-                else
-                    idx[i]=1
-                end
-            end
-            year=yr0[idx]
-            ir=ir0[idx]*100
-            ρ=ρ0[idx]
-            ddata=[0.0, 16.3, 38.3, 43.0, 55.8, 62.2, 66.5, 68.0, 72.6, 71.5]'/100
-            yt=[1960, 1970, 1977, 1983, 1989, 1992, 1995, 1998 ,2001 ,2004]'
-        elseif flag==2 #AUSTRALIA
-            yr0=ir_aus[:,1]
-            if irflag==2
-                ir0=ir_aus[:,3] #DLM of actual inflation
-            else
-                ir0=ir_aus[:,4] #Term structure based
-            end
-            ρ0=ir_aus[:,7]
-            idx=Vector{Bool}(undef,length(ir0))
-            for i in 1:length(ir0)
-                if (isnan(ρ0[i])|isnan(ir0[i]))
-                    idx[i]=0
-                else
-                    idx[i]=1
-                end
-            end
-            year=yr0[idx]
-            ir=ir0[idx]*100
-            ρ=ρ0[idx] 
-            ddata=[0 0.520354595 0.537666422 0.57032816 0.594028535 0.622316221 0.65072652 0.687205136 0.694317256 0.73605656 0.760156768 0.792611398 0.841961623 0.878037024 0.905946326 0.90592637 0.897747613 0.904361411 0.90064194]'
-            yt=vcat([1965], collect(1994:2011))  
-        else #UK
-            yr0=ir_uk[:,1]
-            if irflag==2
-                ir0=ir_uk[:,3]
-            else
-                ir0=ir_uk[:,4]
-            end
-            ρ0=ir_uk[:,6]
-            idx=Vector{Bool}(undef,length(ir0))
-            for i in 1:length(ir0)
-                if (isnan(ρ0[i])|isnan(ir0[i]))
-                    idx[i]=0
-                else
-                    idx[i]=1
-                end
-            end
-            year=yr0[idx]
-            ir=ir0[idx]*100
-            ρ=ρ0[idx]
-            year=year[5:end]
-            ir=ir[5:end]
-            ρ=ρ[5:end]
-            ddata=[0 0.5 0.53 0.65 0.62 0.64 0.62 0.61  0.6]'
-            yt=[1965 1999  2000  2005  2009  2010  2011  2012  2013]'
-        end
+        ddata=Vector{Float64}(vec(ddata))
+        yt=Vector{Int64}(vec(yt))
+        this=new()
+        this.year=year
+        this.ir=ir
+        this.ρ=ρ
+        this.ddata=ddata
+        this.yt=yt
+        return this
     end
-    ddata=Vector{Float64}(vec(ddata))
-    yt=Vector{Int64}(vec(yt))
-    this=new()
-    this.year=year
-    this.ir=ir
-    this.ρ=ρ
-    this.ddata=ddata
-    this.yt=yt
-    return this
-end
 end
 
 
@@ -600,7 +602,7 @@ function figsub5(irflag::Int64 , flag::Int64, model::Int64)
     betasvθt=ladvθt["betas"]
     fvθt(x)=betasvθt[1].+betasvθt[2].*x.+betasvθt[3].*x.^2
     vθt=[fvθt(x) for x ∈ minimum(normyear):0.01:maximum(normyear)]
-    p2=plot(xgrid,vθt,color=:red,label="fitted value",ylims=(0,1))
+    p2=plot(xgrid,vθt,color=:red,label="fitted value",ylims=(0,1),xlabel="year",ylabel="\\theta")
     scatter!(p2,data.year,vθ,color=:red,shape=:cross,label=false)
     p=plot(p1,p2,layout=(2,1))
 return p
@@ -655,7 +657,7 @@ function figsubA2(irflag::Int64 , flag::Int64, model::Int64)
     xgrid=minimum(data.ρ):0.001:maximum(data.ρ)
     p1=plot(xgrid,ρt,color=:red,label="Regression Line",xlabel="\\rho : Data",ylabel="\\rho : Model")
     scatter!(p1,data.ρ,vρ,color=:blue, label=false)
-    p2=scatter(vρ1,vθ1,label ="Corr.Coef. before 1990=$cor1",ylim=(0,1))
+    p2=scatter(vρ1,vθ1,label ="Corr.Coef. before 1990=$cor1",ylim=(0,1),xlabel="Simulated \\rho", ylabel="Simulated \\theta")
     scatter!(vρ2,vθ2, label= "Corr.Coef. after 1990=$cor2", color=:red,shape=:x)
     p=plot(p1,p2,layout=(2,1))
 return p
@@ -752,7 +754,7 @@ function figsubD2(irflag,flag,model)
 end
 
 """
-figD4(x)
+figD2(x)
 
 Creates figure D2
 
@@ -782,7 +784,8 @@ Creates figure A4
 
 """
 function figA4(x)
-    dict=matread(joinpath("atm_cic.mat"))
+    atmcicmat= joinpath(@__DIR__,"data","atm_cic.mat")
+    dict=matread(atmcicmat)
     can=dict["ca_atm"]
     uk=dict["uk_atm"]
     usa=dict["us_atm"]
@@ -814,7 +817,8 @@ Creates figure A5
 """
 
 function figA5(x)
-    dict=matread(joinpath("atm_cic.mat"))
+    atmcicmat= joinpath(@__DIR__,"data","atm_cic.mat")
+    dict=matread(atmcicmat)
     cr=dict["cr"]
     yr=cr[:,1]
     crc=cr[:,2]
@@ -830,7 +834,8 @@ Creates figure D1
 
 """
 function figD1(x)
-    dictir=matread(joinpath("ir.mat"))
+    datair= joinpath(@__DIR__,"data","ir.mat")
+    dictir=matread(datair)
     usa=dictir["ir_usa"]
     yearusa=usa[:,1]
     ir1usa=usa[:,2]
@@ -1338,7 +1343,44 @@ function Table1(x)
             model=1
         end
         for country in 0:3
-            (α[country*3+mp],s[country*3+mp],Xstar[country*3+mp],rmsd[country*3+mp],cor_rho[country*3+mp],cor_theta1[country*3+mp],cor_theta2[country*3+mp],qstar[country*3+mp],θ[country*3+mp])=Calibrate(1,country,model)
+            (α[country*3+mp],s[country*3+mp],Xstar[country*3+mp],rmsd[country*3+mp],cor_rho[country*3+mp],cor_theta1[country*3+mp],cor_theta2[country*3+mp],qstar[country*3+mp],θ[country*3+mp])=round.(Calibrate(1,country,model),digits=3)
+        end 
+    end
+    df.α=α
+    df.s=s
+    df.xstar=Xstar
+    df.NRMSE=rmsd
+    df.cor_ρ=cor_rho
+    df.cor_ρθ_before_1990=cor_theta1
+    df.cor_ρθ_after_1990=cor_theta2
+    df.Implied_qstar=qstar
+    df.θ=θ
+    return df
+end
+
+
+"""
+Table2(x)
+
+Store all results from Table 1 in a DataFrame
+
+"""
+function Table2(x)
+  df=DataFrame()
+  df.Country=["Canada","Canada","Canada","US","US","US","Australia","Australia","Australia","UK","UK","UK"]
+  df.IR_S=["Baseline", "EI-DLM", "EI-TS","Baseline", "EI-DLM", "EI-TS","Baseline", "EI-DLM", "EI-TS","Baseline", "EI-DLM", "EI-TS"]
+  α=zeros(12)
+  s=zeros(12)
+  Xstar=zeros(12)
+  rmsd=zeros(12)
+  cor_rho=zeros(12)
+  cor_theta1=zeros(12)
+  cor_theta2=zeros(12)
+  qstar=zeros(12)
+  θ=zeros(12)
+    for mp in 1:3
+        for country in 0:3
+            (α[country*3+mp],s[country*3+mp],Xstar[country*3+mp],rmsd[country*3+mp],cor_rho[country*3+mp],cor_theta1[country*3+mp],cor_theta2[country*3+mp],qstar[country*3+mp],θ[country*3+mp])=round.(Calibrate(mp,country,1),digits=3)
         end 
     end
     df.α=α
